182cd90cc6511def23530f2a434750fcc52e8743
Emily <92753316+Cookiee20@users.noreply.github.com>
2023-10-17 18:46:18 -0400

    The main driver file for the calculator

---
+++ Main.java
@@ -0,0 +1,40 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+
+
+public class Main extends Application {
+    
+    private static Stage historyStage = null;
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("calculator.fxml"));
+        primaryStage.setTitle("Calculator");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+        
+        createHistoryStage();
+    }
+    
+    public void createHistoryStage() {
+        historyStage = new Stage();
+        historyStage.setTitle("Calculation History");
+        historyStage.setAlwaysOnTop(true);
+        historyStage.setResizable(false);
+        historyStage.initModality(Modality.APPLICATION_MODAL);
+    }
+    
+    public static Stage getHistoryStage() {
+        return historyStage;
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
